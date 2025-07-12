e737f9b11b1c1ea85e3110f1f3a5bfbf71e4e3c4
Yeni Adams <yeni.adams@gmail.com>
2025-07-12 23:31:18 +0200

    netlify security fix

--- bookstore-backend/src/main/java/com/bookstore/bookstore/config/SecurityConfig.java
+++ bookstore-backend/src/main/java/com/bookstore/bookstore/config/SecurityConfig.java
@@ -75,7 +75,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173")); 
+        configuration.setAllowedOrigins(Arrays.asList(
+        "http://localhost:5173", // For local development
+        "https://graceful-cucurucho-63ef2d.netlify.app" // Netlify frontend URL
+    ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
